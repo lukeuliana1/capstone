5dcf63c70af3acacd1390d1e0c76e31eaee59a8a
Daniyar Yeralin <daniyar@client-104-39-95-59.mobility.psu.edu>
2016-03-20 17:57:30 -0400

    Dashboard app

---
+++ capstoneTracker/dashboard/views.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.core.context_processors import csrf
+from django.http import Http404, HttpResponse
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+
+@login_required(login_url='/account/login/')
+def description_page(request):
+    return render_to_response('dashboard/dashboard.html', RequestContext(request))
