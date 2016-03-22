838811a14e45f82448a1377d3c0f44963edcbd48
Daniyar Yeralin <daniyar@Daniyar-Mac.local>
2016-03-22 15:38:58 -0400

    Dashboard UI Enhancements

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -2,6 +2,23 @@ $ = jquery = Jquery = require("jquery");
 require("slimScroll");
 var fullpage = require("fullpage.js");
 
+jQuery.fn.removeClassExcept = function (val) {
+    return this.each(function (index, el) {
+        var keep = val.split(" "),  // list we'd like to keep
+        reAdd = [],          // ones that should be re-added if found
+        $el = $(el);       // element we're working on
+        // look for which we re-add (based on them already existing)
+        for (var c = 0; c < keep.length; c++){
+          if ($el.hasClass(keep[c])) reAdd.push(keep[c]);
+        }
+
+        // drop all, and only add those confirmed as existing
+        $el
+          .removeClass()               // remove existing classes
+          .addClass(reAdd.join(' '));  // re-add the confirmed ones
+    });
+};
+
 $(document).ready(function() {
     $('.slider').fullpage({
     	controlArrows: false,
@@ -11,33 +28,32 @@ $(document).ready(function() {
 
     });
 
-    /*var blockedArray = [$(".description-block"), $(".team-block"), $(".sponsors-block"), $(".github-block")];
-    for(i=0; i < blockedArray.size(); i++){
-        blockedArray[i].slimScroll({
+    var blocksArray = [$(".description-block"), $(".team-block"), $(".sponsors-block"), $(".github-block")];
+    for(i=0; i < blocksArray.length; i++){
+        blocksArray[i].slimScroll({
             height: '89%'
         });
-    }*/
-    $(".description-block").slimScroll({
-        height: '89%'
-    });
-
-    $(".team-block").slimScroll({
-        height: '89%'
-    });
-
-    $(".sponsors-block").slimScroll({
-        height: '89%'
-    });
-
-    $(".github-block").slimScroll({
-        height: '89%'
-    });
+    }
 
     $(".description-section").on("click", function() {
     	$.fn.fullpage.moveTo(0, 0);
+        $(".sub-nav").removeClassExcept("nav sub-nav");
+        $(".sub-nav").addClass("first"); 
     });
     $(".team-section").on("click", function() {
     	$.fn.fullpage.moveTo(0, 1);
+        $(".sub-nav").removeClassExcept("nav sub-nav");
+        $(".sub-nav").addClass("second"); 
+    });
+    $(".sponsors-section").on("click", function() {
+        $.fn.fullpage.moveTo(0, 2);
+        $(".sub-nav").removeClassExcept("nav sub-nav");
+        $(".sub-nav").addClass("third"); 
+    });
+    $(".github-section").on("click", function() {
+        $.fn.fullpage.moveTo(0, 3);
+        $(".sub-nav").removeClassExcept("nav sub-nav");
+        $(".sub-nav").addClass("fourth"); 
     });
     
 });
